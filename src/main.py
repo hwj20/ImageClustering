f5bdca388a89fc7cf18a85e70503f89beae7b5fe
hwj <912299489@qq.com>
2021-04-17 19:54:47 +0800

    find a kmeans source, without testing.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,3 @@
-import matplotlib
-import numpy
 import numpy as np
 import matplotlib.pylab as plt
 import skimage.io as io
@@ -9,7 +7,7 @@ def read_image(path: str):
     coll = io.ImageCollection(path)
     print(len(coll))
     # plt.imshow(coll[0])
-    plt.imshow(numpy.array(coll[0]))
+    plt.imshow(np.array(coll[0]))
     plt.show()
 
 
